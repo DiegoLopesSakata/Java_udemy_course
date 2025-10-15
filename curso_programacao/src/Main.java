98f4d5428c417bfff5deeb7aa612a550e3693670
Diego Lopes Sakata <dihsakata@gmail.com>
2025-10-15 20:06:30 -0300

    learn: primeiro exercicio sobre saida de dados

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -2,12 +2,24 @@ import java.util.Locale;
 
 public class Main{
     public static void main(String[] args){
+        String product1 = "Computer";
+        String product2 = "Office desk";
 
-        double x = 10.35784;
-        System.out.println(x);
-        System.out.printf("%.2f%n", x);
-        System.out.printf("%.4f%n", x);
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100;
+        double price2 = 650.5;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.printf("%s, which price is $ %.2f%n", product1, price1);
+        System.out.printf("%s, which price is $ %.2f%n", product2, price2);
+        System.out.printf("%nRecord: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.printf("%nMeasure with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
         Locale.setDefault(Locale.US);
-        System.out.printf("%.4f%n", x);
+        System.out.printf("US decimal point: %.3f%n", measure);
     }
 }
