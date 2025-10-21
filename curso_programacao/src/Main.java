04101c0e922a6919a2a57d51d4fb879a4cdafd79
Diego Lopes Sakata <dihsakata@gmail.com>
2025-10-21 16:10:56 -0300

    learn: limpa o buffer

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -5,10 +5,20 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        char x;
-        x = sc.next().charAt(0);
+        int x;
+        String s1, s2, s3;
 
-        System.out.println("Voce digitou: " + x);
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
+
+        System.out.println("Voce digitou:");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
 
         sc.close();
     }
