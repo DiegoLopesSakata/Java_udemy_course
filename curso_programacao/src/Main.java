b1abc968d448e33914685aff24224e9dcfffb1eb
Diego Lopes Sakata <dihsakata@gmail.com>
2025-10-28 16:20:04 -0300

    learn: operadores bitwise

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -1,41 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int n1 = 89;
+        int n2 = 60;
 
-        int dia = sc.nextInt();
-        String diaSemana;
-
-        switch (dia){
-            case 1:
-                diaSemana = "Domingo";
-                break;
-            case 2:
-                diaSemana = "Segunda";
-                break;
-            case 3:
-                diaSemana = "Terça";
-                break;
-            case 4:
-                diaSemana = "Quarta";
-                break;
-            case 5:
-                diaSemana = "Quinta";
-                break;
-            case 6:
-                diaSemana = "Sexta";
-                break;
-            case 7:
-                diaSemana = "Sabado";
-                break;
-            default:
-                diaSemana = "Valor invalido";
-                break;
-        }
-
-        System.out.println("O dia da semana eh " + diaSemana);
-        sc.close();
+        System.out.println(n1 & n2);
+        System.out.println(n1 | n2);
+        System.out.println(n1 ^ n2);
     }
 }
