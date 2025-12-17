b3638c7cbee13e88834a45f7b8bef30dcb454439
Diego Lopes Sakata <dihsakata@gmail.com>
2025-12-17 16:54:19 -0300

    learn: metodos estaticos e cria calculator

--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 void main() {
     var sc = new Scanner(System.in);
 
-    var employee = new Employee();
-    employee.name = sc.nextLine();
-    employee.grossSalary = sc.nextDouble();
-    employee.tax = sc.nextDouble();
+    double radius = sc.nextDouble();
+    double circumference = Calculator.circumference(radius);
+    double volume = Calculator.volume(radius);
 
-    double liquidSalary = employee.netSalary();
-
-    System.out.printf("Data: name: %s, grossSalary: $ %.2f, liquidSalary: $ %.2f\n", employee.name, employee.grossSalary, liquidSalary);
-
-    employee.increaseSalary(25);
+    System.out.printf("Circumference: %.2f\n", circumference);
+    System.out.printf("Volume: %.2f\n", volume);
+    System.out.printf("PI Value: %.2f", Calculator.PI);
 
     sc.close();
 }
