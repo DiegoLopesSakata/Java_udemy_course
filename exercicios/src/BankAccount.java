1ce330390e2284ae652cd7e474ac66d9c7b0a433
Diego Lopes Sakata <dihsakata@gmail.com>
2026-01-27 17:43:33 -0300

    learn: encapsulamento e sobrecarga

---
+++ exercicios/src/BankAccount.java
@@ -0,0 +1,53 @@
+public class BankAccount {
+    private int accountNumber;
+    private String holderName;
+    private float balance;
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public BankAccount (int accountNumber, String holderName){
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        balance = 0;
+    }
+
+    public BankAccount (int accountNumber, String holderName, float value){
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        balance = value;
+    }
+
+    public void deposit(float value){
+        balance+=value;
+    }
+
+    public boolean withdraw(float value){
+        if(value > balance) return false;
+
+        balance-=value;
+        return true;
+    }
+
+    public String toString(){
+        return "Account "
+                + accountNumber
+                + ", Holder: "
+                + holderName
+                + ", Balance: $ "
+                + String.format("%.2f", balance);
+    }
+}
--- exercicios/src/Main.java
+++ exercicios/src/Main.java
@@ -1,15 +1,52 @@
+import javax.naming.ldap.BasicControl;
+import javax.xml.transform.Source;
+import java.awt.image.BandCombineOp;
 import java.util.Scanner;
 
 void main() {
     var sc = new Scanner(System.in);
 
-    double radius = sc.nextDouble();
-    double circumference = Calculator.circumference(radius);
-    double volume = Calculator.volume(radius);
+    int accountNumber;
+    String name;
+    float value;
+    char choice;
 
-    System.out.printf("Circumference: %.2f\n", circumference);
-    System.out.printf("Volume: %.2f\n", volume);
-    System.out.printf("PI Value: %.2f", Calculator.PI);
+    System.out.print("Enter account number: ");
+    accountNumber = sc.nextInt();
+    System.out.print("Enter account holder: ");
+    sc.nextLine();
+    name = sc.nextLine();
+    System.out.print("Is there an initial deposit (y/n)? ");
+    choice = sc.next().charAt(0);
+
+    if(choice == 'y'){
+        System.out.print("Enter inital deposit value: ");
+        value = sc.nextFloat();
+    }
+    else{
+        value = 0;
+    }
+
+    var account = new BankAccount(accountNumber, name, value);
+
+    System.out.println("Account data:");
+    System.out.println(account);
+
+    System.out.println("\nEnter a deposit value: ");
+    value = sc.nextFloat();
+    account.deposit(value);
+
+    System.out.println("Updated account data:");
+    System.out.println(account);
+
+    System.out.println("\nEnter a withdraw value: ");
+    value = sc.nextFloat();
+    if(!account.withdraw(value)){
+        System.out.println("Withdraw value is bigger than account balance.");
+    }
+
+    System.out.println("Updated account data:");
+    System.out.println(account);
 
     sc.close();
 }
